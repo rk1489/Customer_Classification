b3e350fa3cecfb33d60e199bd0555b5bab8b1dba
Raushan Kumar <69127358+rk1489@users.noreply.github.com>
2021-05-26 22:38:42 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,42 +1,12 @@
-import pandas as pd
-
-df = pd.read_csv('Customer_Class.csv')
-
-# Defining data and label
-X = df.iloc[:, 1:5]
-y = df.iloc[:, 5]
-
-# Spliting data into training and test datasets
-from sklearn.model_selection import train_test_split
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=1)
-
-# Applying KNN algorithm
-from sklearn.neighbors import KNeighborsClassifier
-
-knn = KNeighborsClassifier(n_neighbors = 7, p = 2, metric='minkowski')
-knn.fit(X_train, y_train)
-
-#Applying SVC algorithm
-from sklearn.svm import SVC
-
-svm = SVC(kernel='rbf', random_state=0, gamma=.10, C=1.0)
-svm.fit(X_train, y_train)
-
-# Applying Decision Tree
-from sklearn import tree
-
-decision_tree = tree.DecisionTreeClassifier(criterion='gini')
-decision_tree.fit(X_train, y_train)
-
-# Using Random Forest
-from sklearn.ensemble import RandomForestClassifier
-
-random_forest = RandomForestClassifier()
-random_forest.fit(X_train, y_train)
-
-
 import streamlit as st
+import pickle
+
+
+
+knn_model=pickle.load(open('knn_model.pkl','rb'))
+svm_model=pickle.load(open('svm_model.pkl','rb'))
+tree_model=pickle.load(open('tree_model.pkl','rb'))
+rf_model=pickle.load(open('rf_model.pkl','rb'))
 
 def classify(num):
     if num == 0:
@@ -74,13 +44,13 @@ def main():
     
     if st.button('Classify'):
         if option=='K - Nearest Neighbour (Accuracy = 97.5%)':
-            st.success(classify(knn.predict(inputs)))
+            st.success(classify(knn_model.predict(inputs)))
         elif option=='Support Vector Classifier (Accuracy = 38.8%)':
-            st.success(classify(svm.predict(inputs)))
+            st.success(classify(svm_model.predict(inputs)))
         elif option=='Decision Tree (Accuracy = 95.0%)':
-            st.success(classify(decision_tree.predict(inputs)))
+            st.success(classify(tree_model.predict(inputs)))
         elif option=='Random Forest (Accuracy = 96.3%)':
-            st.success(classify(random_forest.predict(inputs)))
+            st.success(classify(rf_model.predict(inputs)))
             
 
 if __name__=='__main__':
